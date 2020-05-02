a902d675706c59b607ee8463f3b3682d71688baf
IsabelSonnet <izzysonnet@gmail.com>
2020-05-01 21:37:41 -0700

    lads we have a rope and stick figure that move

--- tow_script.js
+++ tow_script.js
@@ -1,3 +1,20 @@
 $(document).ready(() => {
 
+    var mtow = $("#movingtow");
+
+    $(document).keydown(function (event) {
+        switch (event.which) {
+            case 37:
+                $(mtow).finish().animate({
+                    left: "-=20"
+                });
+                break;
+            case 39:
+                $(mtow).finish().animate({
+                    left: "+=20"
+                });
+                break;
+        }
+    });
+
 });
