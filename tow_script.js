8961f76327c51a3b579c09ad58ab78ba384080b0
IsabelSonnet <izzysonnet@gmail.com>
2020-05-07 13:20:12 -0700

    so currently the collision algorithm is working but not right

--- tow_script.js
+++ tow_script.js
@@ -2,6 +2,10 @@ $(document).ready(() => {
 
     var mtow = $("#movingtow");
 
+    let $line = $("#line");
+    let $rplayer = $("#rplayer");
+    let $lplayer = $("#lplayer");
+
     $(document).keydown(function (event) {
         switch (event.which) {
             case 37:
@@ -15,27 +19,39 @@ $(document).ready(() => {
                 });
                 break;
         }
-    });
 
-    let line = $("#line");
-    let rplayer = $("#rplayer");
-    let lplayer = $("#lplayer");
-
-    $(document).keydown(function (event) {
         if (collision($rplayer, $line) == true) {
+            $("#lwinner").show();
             console.log("collision!");
         } else {
             console.log("no collision!");
+            $("#lwinner").hide();
+        }
+
+        if (collision($lplayer, $line) == true) {
+            $("#rwinner").show();
+            console.log("collision!");
+        } else {
+            console.log("no collision!");
+            $("#rwinner").hide();
         }
     });
 
+ //   $(document).keydown(function (event) {
+ //       if (collision($rplayer, $line) == true) {
+ //           $("#winner").show();
+ //           console.log("collision!");
+ //       } else {
+ //           console.log("no collision!");
+ //           $("#winner").hide();
+ //       }
+ //   });
 
-    // Here is the collision algorithm
-    function collision(R, l) {
-        if (R.position().left < l.position().left + l.width() &&
-            R.position().left + R.width() > l.position().left &&
-            R.position().top < l.position().top + l.height() &&
-            R.position().top + R.height() > l.position().top)
+    function collision(p, l) {
+        if (p.position().left < l.position().left + l.width() &&
+            p.position().left + p.width() > l.position().left &&
+            p.position().top < l.position().top + l.height() &&
+            p.position().top + p.height() > l.position().top)
         {
             return true;
         } else {
