2206f239a59c27afc8b324c70b1c98a0a8458f18
IsabelSonnet <izzysonnet@gmail.com>
2020-05-10 20:01:33 -0700

    um this is the "a note" again

--- tow_script.js
+++ tow_script.js
@@ -2,12 +2,9 @@ $(document).ready(() => {
 
     var mtow = $("#movingtow");
 
-    let $mtowDiv = $("#movingtow");
     let $line = $("#line");
     let $rplayer = $("#rplayer");
     let $lplayer = $("#lplayer");
-
-    showGame();
 
     $(document).keydown(function (event) {
         switch (event.which) {
@@ -15,80 +12,32 @@ $(document).ready(() => {
                 $(mtow).finish().animate({
                     left: "-=20"
                 });
-//                if (collision($rplayer, $line) == true) {
-//                    $("#lwinner").show();
-//                    console.log("collision!");
-//                    hideGame();
-//                } else {
-//                    console.log("no collision!");
-//                    $("#lwinner").hide();
-//                    showGame();
-//                }
+                if (collision($rplayer, $line) == true) {
+                    $("#lwinner").show();
+                    console.log("collision!");
+                    hideGame();
+                } else {
+                    console.log("no collision!");
+                    $("#lwinner").hide();
+                    showGame();
+                }
                 break;
             case 39:
                 $(mtow).finish().animate({
                     left: "+=20"
                 });
-//                if (collision($lplayer, $line) == true) {
-//                    $("#rwinner").show();
-//                    console.log("collision!");
-//                    hideGame();
-//                } else {
-//                    console.log("no collision!");
-//                    $("#rwinner").hide();
-//                    showGame();
-//                }
+                if (collision($lplayer, $line) == true) {
+                    $("#rwinner").show();
+                    console.log("collision!");
+                    hideGame();
+                } else {
+                    console.log("no collision!");
+                    $("#rwinner").hide();
+                    showGame();
+                }
                 break;
         }
     });
-
-
-    $(document).keydown(function (event) {
-        console.log($rplayer)
-        console.log($lplayer)
-        console.log($lplayer)
-
-        if (hasRightLost($mtowDiv, $rplayer, $line) == true) {
-            console.log("collision!");
-            $("#lwinner").show();
-            $("#rwinner").hide();
-        } else {
-            console.log("no collision!");
-        }
-
-        if (hasLeftLost($mtowDiv, $lplayer, $line) == true) {
-            console.log("collision!");
-            $("#rwinner").show();
-            $("#lwinner").hide();
-        } else {
-            console.log("no collision!");
-        }
-    });
-
-    function hasRightLost(m, p, l) {
-        console.log("checking if right has lost")
-        console.log("tow right: ", m.position().left + m.width(), " player width: ", p.width(),  " line: ", l, " difference: ", ( m.position().left + m.width() -p.width()) - l)
-        if (( m.position().left + m.width() -p.width()) < (l.position().left+(0.5*l.width())))
-        {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-
-    function hasLeftLost(m, p, l) {
-        console.log("checking if left has lost")
-        console.log("tow left: ", m.position().left, " player width: ", p.width(),  " line: ", l, "difference: ", (m.position().left + p.width()) - l)
-        if (( m.position().left + p.width())  > (l.position().left+(0.5*l.width())))
-        {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 
     $("#startGame").click(function () {
         $("#instructions").hide();
@@ -113,12 +62,19 @@ $(document).ready(() => {
         $("#countdown").show();
     }
 
+    function collision(p, l) {
+        if (p.position().left < l.position().left + l.width() &&
+            p.position().left + p.width() > l.position().left &&
+            p.position().top < l.position().top + l.height() &&
+            p.position().top + p.height() > l.position().top)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-
-
-
-
-    var remaining = 40;
+    var remaining = 40; //we need to find a way to wait to start the timer until the start game button is pressed and finish when the collision happens
     var timer = setInterval(function () {
         if (remaining <= 0) {
         clearInterval(timer);
@@ -130,4 +86,3 @@ $(document).ready(() => {
     }, 1000);
 
 });
-
